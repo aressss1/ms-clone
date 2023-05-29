0b822f5dee90dc0aa842964950452d1f68258753
aressss1 <codewithfaizi@gmail.com>
2023-05-29 13:41:37 +0500

    day-4

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,17 +2,21 @@
 
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGoogle, BsGithub } from "react-icons/bs"
 import axios from "axios";
 import { toast } from "react-hot-toast"
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from "next/navigation";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+    const session = useSession() 
+    const router = useRouter()
+
     const [variant, setVariant] = useState<Variant>("LOGIN")
 
     const [isLoading, setIsLoading] = useState(false)
@@ -24,6 +28,13 @@ const AuthForm = () => {
             setVariant("LOGIN")
         }
     }, [variant])
+
+    useEffect(() => {
+        if(session?.status === 'authenticated'){
+            router.push('/users')
+        }
+    } , [session?.status , router])
+
 
     const {
         register,
@@ -60,6 +71,7 @@ const AuthForm = () => {
 
                     if (callback?.ok && !callback?.error) {
                         toast.success("Logged in!")
+                        router.push('/users')
                     }
                 })
                 .finally(() => setIsLoading(false))
